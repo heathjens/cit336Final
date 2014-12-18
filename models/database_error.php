79cb24e461b4929ff4042bdccbe0aa746c0f1364
Heather <hejclan@gmail.com>
2014-12-18 01:13:33 -0700

    Create database_error.php

---
+++ models/database_error.php
@@ -0,0 +1,21 @@
+<body>
+    <div id="page">
+        <div id="header">
+            <h1>Inspiration</h1>
+        </div>
+
+        <div id="main">
+            <h1>Database Error</h1>
+            <p>There was an error connecting to the database.</p>
+            <p>Error message: <?php echo $error_message; ?></p>
+            <p>&nbsp;</p>
+        </div><!-- end main -->
+
+        <div id="footer">
+            <p class="copyright">
+                &copy; <?php echo date("Y"); ?> 
+            </p>
+        </div>
+
+    </div><!-- end page -->
+</body>
