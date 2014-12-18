c9bdcfd2d44288b169c201ab40237da48897a11e
Heather <hejclan@gmail.com>
2014-12-18 01:23:28 -0700

    Create service.php

---
+++ views/service.php
@@ -0,0 +1,51 @@
+
+        <main>
+            <h1>Services</h1>
+            <h2>The Six Step Process</h2>
+            <p>We want you to love your website so we have the customer approve each step of the process.</p>
+            <ul>
+                <li>About your Business
+                    <ul><li>Purpose of Website</li>
+                        <li>Goals of the Website</li>
+                        <li>Target Audience</li>
+                        <li>Content</li>
+                    </ul>
+                </li>
+                <li>Planning
+                    <ul>
+                        <li>Site Map</li> 
+                        <li>Navigation Bar</li>
+                        <li>Headings and Sub-headings</li>
+                        <li>Technolgy Needed</li>
+                    </ul>
+                </li>
+                <li>Design
+                    <ul>
+                        <li>Design logo (if needed)</li> 
+                        <li>2-3 mock-ups</li>
+                    </ul>
+                </li>
+                <li>Develop
+                    <ul>
+                        <li>This is where we create the website</li>
+                    </ul>
+                </li>
+                <li>Testing & Delivery
+                    <ul>
+                        <li>Test in different browsers</li> 
+                    </ul>
+                </li>
+                <li>Maintenance
+                    <ul>
+                        <li>We offer monthly maintenance packages!</li>
+                    </ul>
+                </li>
+            </ul>
+            <p>Part of our desire at Inspired Web Design is to give you the tools and 
+                information to help you make decisions. We are always looking for information 
+                about the latest trends in web design. We want to help business 
+                owners make informed decisions.</p>
+
+            <a href="/views/related-articles.php">Related Articles</a>
+        </main>
+    
