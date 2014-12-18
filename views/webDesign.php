d6f198491b59bdefbfbe0b1e2a2594d59f7e0dfa
Heather <hejclan@gmail.com>
2014-12-18 01:26:05 -0700

    Create webDesign.php

---
+++ views/webDesign.php
@@ -0,0 +1,30 @@
+
+        <main>
+            <h1>Portfolio</h1>
+            <p>There are many kind of sites. Deciding on the right site for your 
+                business is an important decision that starts with what your 
+                goal is for your website. The first question is what is your purpose 
+                for creating this site? Is it to increase your business? Is it to 
+                educate others? Once you have made this decision creating your site 
+                becomes much easier. </p>
+
+            <blockquote>Which site is right for your business?</blockquote>
+            <h2>Examples of different sites:</h2>
+            <div>
+                <a href="http://americanantiquetoys.theinspiredwebdesign.com"><img src="/images/americantoys.png" alt="American Antique Toys" class="page"></a>
+                <a href="http://cookingformany.com"><img src="/images/cooking.png" alt="Cooking For Many" class="page"></a>
+                <a href="http://theinspiredwebdesign.com/triathlon/"><img src="/images/triathlon.png" alt="Prison to Prison Triathlon" class="page"></a>
+                <a href="http://busybee.theinspiredwebdesign.com"><img src="/images/busy.png" alt="Busy Bee Photography" class="page"></a>
+
+            </div>
+            <br><br><br><br>
+
+            <div class="space">
+                <h2 class="heading">Examples of Design and Development:</h2>
+                <ul>
+                    <li><a href="/views/graphic.php">Graphic Design Work</a></li>
+                    <li><a href="/views/programming.php">Programs</a></li>
+                </ul>
+            </div>
+        </main>
+     
