e5ffef8412d892ea82bb063c6a08707ba97beba3
Heather <hejclan@gmail.com>
2014-12-18 01:17:45 -0700

    Create about.php

---
+++ views/about.php
@@ -0,0 +1,12 @@
+
+           <main>
+            <h1>About Me</h1>
+            <h2>Heather Jensen</h2>
+            <img src="/images/me.jpg" alt="Heather Jensen" class="picture">
+            <p>I am currently working on my Bachelor's Degree in Web Design and 
+            Development with an emphasis in Design at Brigham Young University- Idaho.
+            I plan to graduate in the spring of 2016. I love designing a dynamic website
+            with an aesthetically pleasing layout. I enjoy being able to think my way out of 
+            problems and the puzzle that is web design. In my free time, 
+            I enjoy being outdoors, reading the classics, and spending time with my family.</p>
+        </main>
