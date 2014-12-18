9703842a08463f4bbf17726c3e548de1809207ed
Heather <hejclan@gmail.com>
2014-12-18 01:21:47 -0700

    Create newitem.php

---
+++ views/newitem.php
@@ -0,0 +1,14 @@
+<?php ?>
+
+        <main>
+            <div id="additem">
+                <form method="POST" action="/?action=newitemsubmit">
+                    <fieldset>
+                        <legend>Add to our inspiration gallery.</legend>
+                        Name:<input type="text" name="Name" ><br>
+                        Image Url:<input type="text" name="imageURL" ><br>
+                        <input type="submit" value="Submit" name="Submit" >
+                    </fieldset>
+                </form>
+            </div>
+        </main>
