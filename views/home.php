d417bfbe073dae00a105aa3aff58b1e6a9e880e2
Heather <hejclan@gmail.com>
2014-12-18 01:19:55 -0700

    Create home.php

---
+++ views/home.php
@@ -0,0 +1,21 @@
+ <main>
+
+            <div>
+                <img src="/images/light-bulb2.png" alt="artwork" class="page">
+            </div>
+            <h2>You Know your Business</h2>
+            <p>We believe a website should increase your image and branding. It should
+                be a natural extension to the overall theme, look, and feel of your business.
+                For this reason, we learn as much as possible about your business and your goals.
+
+            </p>
+            <h2>We Know Design</h2>
+            <p>You know your business, we know websites. We take your ideas and build
+                a dynamic website that looks great on any cell phone, tablet, or computer.
+                Within the next five years, internet usage on mobile phones will outpace
+                computers. People are researching and buying from a multitude of devices so having
+                a website that works and looks good on any electronic device is imperative.
+                At Inspired Web Design, we listen to our client's ideas and make them a reality. </p>
+
+           
+        </main>
