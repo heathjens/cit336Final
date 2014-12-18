a88eb9362bf7612cdc8259200b9dcd24d5dddd23
Heather <hejclan@gmail.com>
2014-12-18 01:24:01 -0700

    Create site-plan.php

---
+++ views/site-plan.php
@@ -0,0 +1,125 @@
+
+        <main>
+            <h1>Style Guide</h1>
+            <h2>Purpose</h2>
+            <p>I am designing an informational website to showcase my work in both design 
+                and the web. This site shows the diversity of my work. This site is clear,
+                organized, and creative. This site is important for my continual growth and 
+                learning and to show others the importance of my work. This site is used 
+                to show potential clients my work. I am designing an informational website
+                to showcase my work in both design and the web. This site shows the diversity
+                of my work. This site is clear, organized, and creative. This site is 
+                important for my continual growth and learning and to show others the 
+                importance of my work. This site is used to show potential clients my work. </p>
+
+            <h2>Target Audience</h2>
+            <p>The target audience for my site are business owners. They work for a living
+                and time is money. They are between the ages of 30-40. They are self- motivated,
+                frugal, hard- working, passionate, and usually self-educated. They know that
+                the more successful their business is the better they can take care of their
+                family, so there is always a lot at risk. They understand how to use and 
+                operate a computer and the internet. They usually know what they like and 
+                can make decisions well. </p>
+
+            <p>This site is for business owners who are seeking graphic design, web design, or 
+                web development help. Most of the site is geared to sell a product, but some of it is 
+                geared to help business owners find inspiration or information so that they can make 
+                more informed decisions. </p>
+
+            <h2>Personas</h2>
+            <img src="/images/maleBusiness.jpg" alt="Male Business Owner" class="picture">
+            <p>John Smith owns a real estate company. He is always looking for ways to 
+                market his company and increase business. He is between the ages of 30-40. He 
+                is married with a family. Work is hard and, at times, days are long. He works hard
+                so that him and his family will be able to have everything they need and want so 
+                increasing business through a dynamic, mobile-responsive website is very appealing. </p>
+            <br><br><br><br><br>
+            <img src="/images/femaleBusiness.jpg" alt="Female Business Owner" class="picture">
+            <p>Jenny White owns a bakery. She spends her time baking and running a business not 
+                creating websites. She wants to know that her business is on the internet, but 
+                does not want to worry about the day-to-day management of the site. She is between
+                the ages of 30-40, she knows that the internet is important for increasing her business.
+                She wants customers to be able to place orders and see specials from anywhere. </p>
+            <br><br><br>
+
+            <h2>Scenarios</h2>
+            <ul>
+                <li> Business owner looking for a way to better market their products and business
+                    on the internet.</li>
+                <li>Age: 30-40</li>
+                <li>Education:Often self-educated, but some have Bachelors</li>
+                <li>Economic range: Middle class</li>
+                <li>Internet Devices: Phone, tablet, computer</li>
+            </ul>
+
+            <h2>Asset List</h2>
+            <ol>
+                <li>Home page: Has primary and secondary navigation, login for adding inspiration, displays top inspiration. </li>
+                <li>Services page: Walks client through how a website is created. </li>
+                <li>Related Articles page: Has many great articles siting the importance of an online presence for business owners.</li>
+                <li>Web Design page: Displays portfolio of websites that I have created. </li>
+                <li>Graphic Design page: Displays portfolio of graphic design images that have been created. </li>
+                <li>Programming page: Displays different levels of code. </li>
+                <li>Gallery page: Displays inspirational quotes and pictures that can be viewed and voted on. </li>
+                <li>Contact page: A basic form that e-mail Inspired Web Design with questions and interest.</li>
+                <li>About page: Gives a brief bio on the owner of Inspired Web Design.</li>
+                <li>Login page: Allows viewer to login and add inspiration to the site or vote on existing inspiraton. </li>
+                <li>Menu page: Allow user to add items, like items, or update information</li>
+                <li>New Item page: Allows user to add a new item to the gallery.</li>
+                <li>Site plan page: Current page which allows viewers to get a sense of the entire site.</li>
+                <li>Teaching presentation page: Displays teaching presentations and exams for CIT 336. </li>
+                <li>Source code: Links viewer to my Github account. </li>
+            </ol>
+            <h2>Color Scheme</h2>
+            <ul>
+                <li>Border:#c5c7cc </li>
+                <li>Navigation Bar: #d1d7e8</li>
+                <li>Font: #000000</li>
+                <li>Accent:  #ffff61</li>
+            </ul>
+            <h2>Font</h2>
+            <ul>
+                <li>Headers:font-family:bodoni, times, serif;</li>
+                <li>Body copy:font-family: futura, arial, sans-serif;</li>
+            </ul>
+            <h2>Navigation</h2>
+            <ul>
+                <li>Navigation Bar:
+                    <ul>
+                        <li><a href="/index.php">Home</a></li>
+                        <li><a href="/views/services.php">Services</a>
+                            <ul>
+                                <li><a href="/views/related-articles.php">Related Articles</a></li>
+                            </ul></li>
+                        <li><a href="/views/webDesign.php">Portfolio</a>
+                            <ul>
+                                <li><a href="/views/graphic.php">Graphic Design Portfolio</a></li>
+                                <li><a href="/views/webDesign.php">Web Design</a></li>
+                                <li><a href="/views/programming.php">Programming Portfolio</a></li>
+                            </ul></li>
+                        <li><a href="/views/contact.php">Contact</a>
+                            <ul><li><a href="/views/aboutMe.php">About</a></li></ul></li>
+                        <li><a href="/views/login.php">Login</a></li>
+                    </ul></li>
+                <li>Footer: 
+                    <ul>
+                        <li><a href="/views/teaching-present.php">Teaching Presentation</a></li>
+                        <li><a href= "/views/site-plan.php">Site Plan</a></li>
+                        <li><a href="https://github.com/heathjens/cit336.git">Source Code</a></li>
+                    </ul>  
+                </li>
+            </ul>
+
+
+            <h2>Responsiveness</h2>
+            <div>Mobile page:
+                <img src="/images/mobileHome.jpeg" alt="Mobile Responsive page" class="picture">         
+            </div>
+            <div>Tablet page:
+                <img src="/images/tablet.jpeg" alt="Tablet Responsive page" class="picture">
+            </div>
+            <div>Personal Computer:
+                <img src="/images/detail-achieved.png" alt="Personal Computer" class="picture">                
+            </div>
+        </main>
+    
