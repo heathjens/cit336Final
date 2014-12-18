660cca1d2e1c25991ef81db6eed5a51b300a89e0
Heather <hejclan@gmail.com>
2014-12-18 01:19:33 -0700

    Create graphic.php

---
+++ views/graphic.php
@@ -0,0 +1,18 @@
+
+        <main>
+            <h1>Graphic Design Portfolio</h1>
+            <p>Here is some of my past work.</p>
+
+            <h2>Event Posters</h2>
+            <img src="/images/poster-1.jpg" alt="Museum exhibit opening" class="site">
+            <img src="/images/poster2.jpg" alt="Museum exhibit opening" class="site">
+            <img src="/images/poster3.jpg" alt="Museum exhibit opening" class="site">
+
+            <img src="/images/banquet2014.png" alt="custom invite" class="site">
+            <img src="/images/P1Flier-HeatherJensen2.jpg" alt="Event Flier" class="site">
+            <img src="/images/projectidea5.jpg" alt="Photodesign" class="site">
+            <img src="/images/prison2-logo.png" alt="Logo for Mock-up site" class="site">
+            <img src="/images/american-logo4.png" alt="American Antique Toys" class="site">
+
+
+        </main>
