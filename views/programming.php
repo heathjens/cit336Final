23e168e46de80da06b839a65633b3919c6e5e63a
Heather <hejclan@gmail.com>
2014-12-18 01:22:41 -0700

    Create programming.php

---
+++ views/programming.php
@@ -0,0 +1,14 @@
+
+        <main>
+            <h1>Development Portfolio</h1>
+            <p>Here is some past work.</p>
+            <div><h2>Code for Windchill Factor</h2>
+                <figure><img src="/images/temp.png" alt="Example of code" class="program"></figure>
+                <figure><img src="/images/code-windchill.png" alt="Visual of code" class="program"></figure></div>
+            <div><h2>Code for Multiplication Table</h2>
+                <figure><img src="/images/multiply.png" alt="Example of code" class="program"></figure>
+                <figure><img src="/images/code-fivetimes.png" alt="Visual of code" class="program"></figure></div>
+            <div><h2>Code for Investment Calculator</h2>
+                <figure><img src="/images/investment.png" alt="Example of code" class="program"></figure>
+                <figure><img src="/images/code-investment.png" alt="Visual of code" class="program"></figure></div>
+        </main>
