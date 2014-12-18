81eee3622e1c1a93a3b9a46ec4a43d69f91188e3
Heather <hejclan@gmail.com>
2014-12-18 01:23:07 -0700

    Create related-articles.php

---
+++ views/related-articles.php
@@ -0,0 +1,14 @@
+
+        <main>
+            <h1>Related Articles</h1>
+            <p>There are many great articles and information to help the small business owner 
+                know why it is important to have a website and what you as a business owner need
+                to know about picking a company to help with your website needs.</p>
+            <p>Here are a couple of great articles to help business owners navigate the web:</p>
+            <ul>
+                <li><a href="http://www.forbes.com/sites/erikaandersen/2013/10/07/how-small-business-owners-are-wrecking-their-own-chances-of-success/">"Small Business Owners are Wrecking their own Chances of Success"</a></li>
+                <li><a href="http://blog.hubspot.com/blog/tabid/6307/bid/50/Four-Ways-the-Internet-Is-Transforming-Small-Business.aspx">"Four Ways the Internet is Transfoming Small Business"</a></li>
+                <li><a href="http://www.marketwatch.com/story/on-the-internet-nobody-knows-youre-a-small-business-2013-12-18">"On the Internet Nobody Knows You're a Small Business"</a></li>
+            </ul>
+        </main>
+    
