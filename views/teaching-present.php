e1a9ecac02d2d6248d9d440e617e19359249ccf3
Heather <hejclan@gmail.com>
2014-12-18 01:25:42 -0700

    Create teaching-present.php

---
+++ views/teaching-present.php
@@ -0,0 +1,10 @@
+
+        <main>
+            <h1>Inspired Web Design</h1>
+            <h2>This page is for my CIT 336 class. </h2>
+            <a href="/images/functions-present.pdf">PHP Functions Presentation</a>
+            <a href="/images/distinctoper-present.pdf">SQL DISTINCT operator</a>
+            <a href="/views/exams/functionsexam/index.php">Functions Exam</a>
+            <a href="/views/exams/dbsqlexam/index.php">Datebase/SQL Exam</a>
+
+        </main>
